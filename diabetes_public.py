5d15ee0a4d460eb3a908246499d42c4a1b4d06f1
SanjuBhaskar1234 <166756549+SanjuBhaskar1234@users.noreply.github.com>
2024-05-01 19:21:24 +0530

    diabetes_public.py

--- diabetes_public.py
+++ diabetes_public.py
@@ -51,7 +51,7 @@ if st.button('Diabetes Test Result'):
     diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
     print(Name, end=" ")
     if (diab_prediction[0] == 1):
-        diab_diagnosis = 'The person is diabetic'
+        diab_diagnosis = Name, 'The person is diabetic'
     else:
         diab_diagnosis = 'The person is not diabetic'
         
